137202a2c1fb2516c4ed9504563ceab30bc66970
candra09 <72352882+candra09@users.noreply.github.com>
2021-06-18 20:51:56 +0700

    Add files via upload

---
+++ app/views/templates/footer.php
@@ -0,0 +1,30 @@
+<section class="container-expand-lg mt-5">
+  <!-- Footer -->
+  <footer class="text-center text-white" style="background-color: rgb(117, 223, 255);">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: CTA -->
+      <section class="">
+        <p class="d-flex justify-content-center align-items-center">
+          <span class="me-3">Made With Love By GTGW</span>
+        </p>
+      </section>
+      <!-- Section: CTA -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgb(94, 217, 255);">
+      © 2021 Copyright:
+      <a class="text-white" href="<?= BASEURL; ?>">goodtalkgoodworld.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</section>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+</body>
+</html>
